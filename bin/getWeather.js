e824afde4190a0db6552c33ed201d345559204e3
MyNodeJs <chenai6@sohu.com>
2016-09-19 10:28:44 +0800

    add url-component

--- bin/getWeather.js
+++ bin/getWeather.js
@@ -5,16 +5,10 @@ var parseString = require('xml2js').parseString;
 var argv = require('yargs').argv;
 var iconv = require('iconv-lite');
 var colors = require('colors');
+var urlComponent = require('url-component');
 
-var str = iconv.encode(argv.city, 'gbk').toString('Hex');
-var arr = [];
-while(str.length) {
-  var substr = str.substring(0, 2);
-  arr.push(substr);
-  str = str.substring(2);
-}
-str = arr.join('%');
-str = '%' + str;
+var str = urlComponent.urlEncode(argv.city, 'gbk');
+
 request('http://php.weather.sina.com.cn/xml.php?city=' + str + '&password=DJOYnieT8234jlsK&day=0', function(error, response, body) {
   if(!error && response.statusCode == 200) {
     parseString(body, function(err, result) {
